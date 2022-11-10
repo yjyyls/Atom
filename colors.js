3789f95a515ccf96f5c6364f79e0ed1075362ce4
Jieun Han <yjyyls66@naver.com>
2022-11-10 15:40:54 +0900

    최종

---
+++ colors.js
@@ -0,0 +1,38 @@
+let Links = {
+  setColor: function (color) {
+    // let alist = document.querySelectorAll('a');
+    // let i = 0;
+    // while(i < alist.length){
+    //   console.log(alist[i]);
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // }
+    $('a').css('color', color);
+  }
+}
+let Body = {
+  setColor:function (color) {
+    //document.querySelector('body').style.color = color;
+    $('body').css('color', color);
+  },
+  setBackgroundColor:function (color) {
+    //document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
+  }
+}
+function nightDayHandler(self) {
+  let target = document.querySelector('body');
+  if(self.value === 'night') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white')
+    self.value = 'day';
+
+    Links.setColor('yellow');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue');
+  }
+}
